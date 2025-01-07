f5f7906d4a68f54957d98170065a20e2615f1cb7
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2025-01-07 07:51:19 +0000

    migrate comments model to database

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 # which is in the same directory as our admin.py file. 
 # If you have multiple models that you want to import, then you can separate them with a comma. 
 # For example, in future topics, you will create a Comment model, which will need to be imported at that point.
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
@@ -20,5 +20,5 @@ admin.site.register(Post)
 
 # admin.site.register method takes only one argument. If you are registering multiple models, 
 # you would need a separate line for each model. 
-
+admin.site.register(Comment)
 
