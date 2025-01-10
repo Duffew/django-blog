8e019dbcc7e9e5b804fad8f57ffdf46b63586759
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2025-01-10 08:34:47 +0000

    import urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 # add a urlpattern for your PostList class-based view named home.
 # As the view is a class, you need an as_view() method, unlike the previous function-based view.
 urlpatterns = [
-    path('', views.PostList.as_view(), name='host'),
+    path("", views.PostList.as_view(), name='home'),
 ]
