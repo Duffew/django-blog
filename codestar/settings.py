4238256fcb8124fea99c990b55462a6ec2651910
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2025-01-20 11:50:47 +0000

    enable serving of static files

--- codestar/settings.py
+++ codestar/settings.py
@@ -34,7 +34,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-duffew-djangoblog-6hz5nry4gss.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -154,6 +155,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # build a path for our subdirectory static so we can link to files in the static directory from a template. 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
