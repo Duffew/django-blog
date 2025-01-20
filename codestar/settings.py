8e1c23642263b9d8e32dc6817669d45b0813137f
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2025-01-20 11:37:03 +0000

    add pagination detail

--- codestar/settings.py
+++ codestar/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-duffew-djangoblog-wnq14032s0o.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-duffew-djangoblog-6hz5nry4gss.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
