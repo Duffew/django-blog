68dc3572d29175a640063f75a655b5d0ebdf1ed7
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2025-01-29 08:26:01 +0000

    add collaboration request form

--- codestar/settings.py
+++ codestar/settings.py
@@ -34,9 +34,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-duffew-djangoblog-cscdrth9yd8.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-duffew-djangoblog-a18z8gj2hmi.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
